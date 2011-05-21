922dbb8465125e5b22eb0ecd187d1e44dc50040c
Dominic Tarr <dominic.tarr@gmail.com>
2011-05-20 18:54:25 -0700

    initial

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('sweet as XYZ 2')
+  res.end();
+
+}).listen(8008);
+
+
